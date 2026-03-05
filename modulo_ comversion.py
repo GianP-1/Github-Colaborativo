e9b25776ab432a25a19a8ba868a882911a2aedf5
David <david.pilo@uip.edu.pa>
2026-03-05 17:30:32 -0500

    Se creo el modulo conversion y se agrego la funcion de conversion de km a m

--- modulo_ comversion.py
+++ modulo_ comversion.py
@@ -1,5 +1,5 @@
 convertir = float(input("Introduce el número a convertir: "))
 
-km_m = convertir * 1000\
+km_m = convertir * 1000
 
 print(f"La convercion de {convertir} kilometros a Metros es: {km_m} Metros")
